9127c902821e5f735befde62bb08fec6d988d2af
leimajinguang <leimajinguang@gmail.com>
2022-02-17 15:34:26 -0500

    test

--- danmu.py
+++ danmu.py
@@ -20,19 +20,19 @@ symbols_punct = r'[、-〿]'
 misc_symbols = r'[ㇰ-ㇿ㈠-㉃㊀-㋾㌀-㍿]'
 ascii_char = r'[ -~]'
 
-text = '初めての駅 自由が丘の駅で、大井町線から降りると、ママは、トットちゃんの手を引っ張って、改札口を出ようとした。ぁゟ゠ヿ㐀䶵一鿋豈頻⺀⿕｟ﾟabc！～、〿ㇰㇿ㈠㉃㊀㋾㌀㍿'
-
-print('Original text string:', text, '\n')
-print('All kanji removed:', remove_unicode_block(kanji, text))
-print('All hiragana in text:', ''.join(extract_unicode_block(hiragana_full, text)))
-
-# for dm in dms:
-#     text = dm.text 
-#     text = ''.join(char for char in text if char.isalnum())
-#     print(text)
-#     f.write(text + ",")
-#     f.write(str(datetime.datetime.fromtimestamp(dm.send_time)))
-#     f.write("\n")
-#     print(dm.dm_time)
-#     print(datetime.datetime.fromtimestamp(dm.send_time))
+for dm in dms:
+    text = dm.text 
+    text = ''.join(char for char in text if char.isalnum())
+    text = re.sub(misc_symbols,'', text)
+    text = re.sub(symbols_punct,'', text)
+    text = re.findall(r'[\u4e00-\u9fff]+', text)
+    text = ''.join(text)
+    print(text)
+    if text == '':
+        continue
+    f.write(text + ",")
+    f.write(str(datetime.datetime.fromtimestamp(dm.send_time)))
+    f.write("\n")
+    #print(dm.dm_time)
+    #print(datetime.datetime.fromtimestamp(dm.send_time))
 f.close()
